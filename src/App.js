f78e6ab1746d764b42a858ade2b144c83cbab213
Phong Pahmm <phongphp03@gmail.com>
2025-03-15 14:47:43 +0700

    update spinner

--- src/App.js
+++ src/App.js
@@ -16,12 +16,14 @@ import { RiWindyLine } from "react-icons/ri";
 import { AiFillThunderbolt } from "react-icons/ai";
 import { FiSunrise, FiSunset } from "react-icons/fi";
 import { FaHand } from "react-icons/fa6";
+import { ClipLoader } from "react-spinners";
 
 const App = () => {
   const [city, setCity] = useState("Hanoi");
   const [weatherDataCurrent, setWeatherDataCurrent] = useState(null);
   const [weatherDataDaily, setWeatherDataDaily] = useState(null);
   const [weatherDataHourly, setWeatherDataHourly] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const [isShowModal, setIsShowModal] = useState(false);
   const [dataModalHourlyDetail, setDataModalHourlyDetail] = useState({});
   const [dataModalDailyDetail, setDataModalDailyDetail] = useState({});
@@ -37,13 +39,15 @@ const App = () => {
 
   const handleSearch = async () => {
     setIsErrorToastShown(false);
+    setIsLoading(true);
     try {
       await fetchWeatherCurrent();
       await fetchWeatherHourly();
       await fetchWeatherDaily();
     } catch (error) {
-      setIsErrorToastShown(false);
       console.error(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -51,7 +55,7 @@ const App = () => {
     if (error.response?.data?.code === 404 && !isErrorToastShown) {
       toast.error(error.response.data.message);
       setIsErrorToastShown(true);
-      throw error;
+      return;
     }
   };
 
@@ -138,176 +142,149 @@ const App = () => {
             <CiSearch />
           </span>
         </div>
-        <div className="weather-content">
-          <div className="current-weather-container">
-            <ToastContainer />
-            {weatherDataCurrent && (
-              <div className="current-info">
-                <div className="generals">
-                  <div className="city">{weatherDataCurrent.name}</div>
-                  <span className="date-time">
-                    {new Date().toLocaleDateString("en-US", {
-                      weekday: "long",
-                      year: "numeric",
-                      month: "long",
-                      day: "numeric",
-                    })}
-                  </span>
-                  <div className="temperature">
-                    <div className="temp-icon">
-                      {weatherDataCurrent.weather &&
-                        weatherDataCurrent.weather[0] && (
-                          <img
-                            src={`https://openweathermap.org/img/wn/${weatherDataCurrent.weather[0].icon}@2x.png`}
-                            alt="weather icon"
-                          />
-                        )}
-                    </div>
-                    <div className="temp-desc">
-                      {weatherDataCurrent.weather &&
-                        weatherDataCurrent.weather[0]?.description}
-                    </div>
-                    <div className="temp-value">
-                      {Math.round(weatherDataCurrent.main.temp)}°C
-                    </div>
-                  </div>
-                </div>
-                <div className="details">
-                  <div className="info-box">
-                    <div className="icon">
-                      <FaHand />
-                    </div>
-                    <div className="value">
-                      {Math.round(weatherDataCurrent.main.feels_like)}°C
-                    </div>
-                    <div className="label">Feels Like</div>
-                  </div>
-
-                  <div className="info-box">
-                    <div className="icon">
-                      <WiHumidity />
-                    </div>
-                    <div className="value">
-                      {weatherDataCurrent.main.humidity}%
-                    </div>
-                    <div className="label">Humidity</div>
-                  </div>
-
-                  <div className="info-box">
-                    <div className="icon">
-                      <RiWindyLine />
-                    </div>
-                    <div className="value">
-                      {weatherDataCurrent.wind.speed} m/s
-                    </div>
-                    <div className="label">Wind Speed</div>
-                  </div>
-
-                  <div className="info-box">
-                    <div className="icon">
-                      <AiFillThunderbolt />
-                    </div>
-                    <div className="value">
-                      {weatherDataCurrent.main.pressure} hPa
-                    </div>
-                    <div className="label">Pressure</div>
-                  </div>
-                  <div className="info-box">
-                    <div className="icon">
-                      <FiSunrise />
-                    </div>
-                    <div className="value">
-                      {new Date(
-                        weatherDataCurrent.sys.sunrise * 1000
-                      ).toLocaleTimeString("en-US", {
-                        hour: "2-digit",
-                        minute: "2-digit",
-                        hour12: true,
-                      })}
-                    </div>
-                    <div className="label">Sunrise</div>
-                  </div>
-
-                  <div className="info-box">
-                    <div className="icon">
-                      <FiSunset />
-                    </div>
-                    <div className="value">
-                      {new Date(
-                        weatherDataCurrent.sys.sunset * 1000
-                      ).toLocaleTimeString("en-US", {
-                        hour: "2-digit",
-                        minute: "2-digit",
-                        hour12: true,
-                      })}
-                    </div>
-                    <div className="label">Sunset</div>
-                  </div>
-                </div>
-              </div>
-            )}
+        {isLoading ? (
+          <div className="loading-container">
+            <ClipLoader color="#36d7b7" size={50} loading={isLoading} />
           </div>
-
-          <div className="forecast-weather-container">
-            <div className="forecast-title">Extended Forecast</div>
-            <div className="forecast-weather-hourly">
-              <div className="forecast-title-hourly">Hourly (Each 3 hours)</div>
-              <div className="forecast-hours">
-                {weatherDataHourly.map((entry, index) => {
-                  const date = new Date(entry.dt * 1000);
-                  const time = date.toLocaleTimeString("en-US", {
-                    hour: "2-digit",
-                    minute: "2-digit",
-                  });
-                  const iconUrl = `https://openweathermap.org/img/wn/${entry.weather[0].icon}@2x.png`;
-                  const description = entry.weather[0].description;
-                  const temp = Math.round(entry.main.temp);
-
-                  return (
-                    <div
-                      className="forecast-hour"
-                      key={index}
-                      onClick={() => {
-                        setDataModalHourlyDetail(entry);
-                        setIsDailyModal(false);
-                        setIsShowModal(true);
-                      }}
-                    >
-                      <div className="time">{time}</div>
-                      <div className="icon">
-                        <img src={iconUrl} alt={description} />
+        ) : (
+          <div className="weather-content">
+            <div className="current-weather-container">
+              <ToastContainer />
+              {weatherDataCurrent && (
+                <div className="current-info">
+                  <div className="generals">
+                    <div className="city">{weatherDataCurrent.name}</div>
+                    <span className="date-time">
+                      {new Date().toLocaleDateString("en-US", {
+                        weekday: "long",
+                        year: "numeric",
+                        month: "long",
+                        day: "numeric",
+                      })}
+                    </span>
+                    <div className="temperature">
+                      <div className="temp-icon">
+                        {weatherDataCurrent.weather &&
+                          weatherDataCurrent.weather[0] && (
+                            <img
+                              src={`https://openweathermap.org/img/wn/${weatherDataCurrent.weather[0].icon}@2x.png`}
+                              alt="weather icon"
+                            />
+                          )}
                       </div>
-                      <div className="description">{description}</div>
-                      <div className="temp">{temp}°C</div>
+                      <div className="temp-desc">
+                        {weatherDataCurrent.weather &&
+                          weatherDataCurrent.weather[0]?.description}
+                      </div>
+                      <div className="temp-value">
+                        {Math.round(weatherDataCurrent.main.temp)}°C
+                      </div>
                     </div>
-                  );
-                })}
-              </div>
+                  </div>
+                  <div className="details">
+                    <div className="info-box">
+                      <div className="icon">
+                        <FaHand />
+                      </div>
+                      <div className="value">
+                        {Math.round(weatherDataCurrent.main.feels_like)}°C
+                      </div>
+                      <div className="label">Feels Like</div>
+                    </div>
+
+                    <div className="info-box">
+                      <div className="icon">
+                        <WiHumidity />
+                      </div>
+                      <div className="value">
+                        {weatherDataCurrent.main.humidity}%
+                      </div>
+                      <div className="label">Humidity</div>
+                    </div>
+
+                    <div className="info-box">
+                      <div className="icon">
+                        <RiWindyLine />
+                      </div>
+                      <div className="value">
+                        {weatherDataCurrent.wind.speed} m/s
+                      </div>
+                      <div className="label">Wind Speed</div>
+                    </div>
+
+                    <div className="info-box">
+                      <div className="icon">
+                        <AiFillThunderbolt />
+                      </div>
+                      <div className="value">
+                        {weatherDataCurrent.main.pressure} hPa
+                      </div>
+                      <div className="label">Pressure</div>
+                    </div>
+                    <div className="info-box">
+                      <div className="icon">
+                        <FiSunrise />
+                      </div>
+                      <div className="value">
+                        {new Date(
+                          weatherDataCurrent.sys.sunrise * 1000
+                        ).toLocaleTimeString("en-US", {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                          hour12: true,
+                        })}
+                      </div>
+                      <div className="label">Sunrise</div>
+                    </div>
+
+                    <div className="info-box">
+                      <div className="icon">
+                        <FiSunset />
+                      </div>
+                      <div className="value">
+                        {new Date(
+                          weatherDataCurrent.sys.sunset * 1000
+                        ).toLocaleTimeString("en-US", {
+                          hour: "2-digit",
+                          minute: "2-digit",
+                          hour12: true,
+                        })}
+                      </div>
+                      <div className="label">Sunset</div>
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
-            {weatherDataDaily && (
-              <div className="forecast-weather-daily">
-                <div className="forecast-title-daily">Daily</div>
-                <div className="forecast-days">
-                  {weatherDataDaily.map((entry, index) => {
+
+            <div className="forecast-weather-container">
+              <div className="forecast-title">Extended Forecast</div>
+              <div className="forecast-weather-hourly">
+                <div className="forecast-title-hourly">
+                  Hourly (Each 3 hours)
+                </div>
+                <div className="forecast-hours">
+                  {weatherDataHourly.map((entry, index) => {
                     const date = new Date(entry.dt * 1000);
-                    const dayOfWeek = date.toLocaleDateString("en-US", {
-                      weekday: "long",
+                    const time = date.toLocaleTimeString("en-US", {
+                      hour: "2-digit",
+                      minute: "2-digit",
                     });
                     const iconUrl = `https://openweathermap.org/img/wn/${entry.weather[0].icon}@2x.png`;
                     const description = entry.weather[0].description;
-                    const temp = Math.round(entry.averageTemp);
+                    const temp = Math.round(entry.main.temp);
 
                     return (
                       <div
-                        className="forecast-day"
+                        className="forecast-hour"
                         key={index}
                         onClick={() => {
-                          setDataModalDailyDetail(entry);
-                          setIsDailyModal(true);
+                          setDataModalHourlyDetail(entry);
+                          setIsDailyModal(false);
                           setIsShowModal(true);
                         }}
                       >
-                        <div className="day-of-week">{dayOfWeek}</div>
+                        <div className="time">{time}</div>
                         <div className="icon">
                           <img src={iconUrl} alt={description} />
                         </div>
@@ -318,9 +295,44 @@ const App = () => {
                   })}
                 </div>
               </div>
-            )}
+              {weatherDataDaily && (
+                <div className="forecast-weather-daily">
+                  <div className="forecast-title-daily">Daily</div>
+                  <div className="forecast-days">
+                    {weatherDataDaily.map((entry, index) => {
+                      const date = new Date(entry.dt * 1000);
+                      const dayOfWeek = date.toLocaleDateString("en-US", {
+                        weekday: "long",
+                      });
+                      const iconUrl = `https://openweathermap.org/img/wn/${entry.weather[0].icon}@2x.png`;
+                      const description = entry.weather[0].description;
+                      const temp = Math.round(entry.averageTemp);
+
+                      return (
+                        <div
+                          className="forecast-day"
+                          key={index}
+                          onClick={() => {
+                            setDataModalDailyDetail(entry);
+                            setIsDailyModal(true);
+                            setIsShowModal(true);
+                          }}
+                        >
+                          <div className="day-of-week">{dayOfWeek}</div>
+                          <div className="icon">
+                            <img src={iconUrl} alt={description} />
+                          </div>
+                          <div className="description">{description}</div>
+                          <div className="temp">{temp}°C</div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
+        )}
         {isShowModal &&
           (isDailyModal ? (
             <ModalDailyDetail
